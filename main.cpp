b71675f64a88d91ec23b0109bc6553e37b567436
Samson Carter <129422671+SamsonCart@users.noreply.github.com>
2024-03-13 15:53:25 -0400

    Update main.cpp
    Changes to SFML format, included mouse press catches, and 2 buttons.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,70 @@
-//
-// This program was written by Samson Carter, Dylan Dixon, and Chance Nahuway.
-//
-
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "TextureManager.h"
 #include "Maze.h"
+
 using namespace std;
 
-void SFML_test_function() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.setFillColor(sf::Color::Cyan);
+int main()
+{
+
+    int width = 600;
+    int height = 600;
+
+    Maze maze(100, 1);
+
+    sf::RenderWindow window(sf::VideoMode(width, height), "Maze Masters (What was our Name??)");
+
+    sf::Sprite startButton(TextureManager::GetTexture("start"));
+    startButton.setPosition(width/2, 500);
+
+    sf::Sprite showButton(TextureManager::GetTexture("show"));
+    showButton.setPosition(width/2, 550);
 
     while (window.isOpen())
     {
         sf::Event event;
-
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            else if (event.type == sf::Event::MouseButtonPressed) {
+
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    auto mousePosition = sf::Mouse::getPosition(window);
+                    //have the maze account for the click if necessary automatically
+                    //maze.mouse_press_left(mousePosition.x, mousePostion.y);}
+
+                    if (startButton.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+                        cout << "1" << endl; //FIXME
+                        //maze.start();
+                        //starts a timer, and allows players to toggle the color of tiles
+                        //DO NOT allow toggle ahead of time!
+
+                    else if (showButton.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+                        cout << "2" << endl; //FIXME
+                        //maze.solve();
+                        //solve the maze using the selected solving algorithm from a drop down menu
+
+            }
+                if (event.mouseButton.button == sf::Mouse::Right) {
+                    auto mousePosition = sf::Mouse::getPosition(window);
+                    //add info if we need a right button press, but i dont think so
+                }
+
+            }
+
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::White);
+
+        window.draw(startButton);
+        window.draw(showButton);
+
+        //maze.draw_maze(window);
         window.display();
     }
-}
 
-int main() {
-    Maze current_maze = Maze(100, 1);
-    SFML_test_function();
     return 0;
 }
